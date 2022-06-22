83898373b876dd2a2a5ee181980336c88ceb34c9
Ashadullah Shawon <ashadullah.shawon@bjitgroup.com>
2022-06-22 11:10:02 +0600

    Added pipelines for GCP

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+#Dockerfile
+FROM python:3.7-slim
+RUN pip install flask
+WORKDIR /app
+COPY app.py /app/app.py
+ENTRYPOINT ["python"]
+CMD ["/app/app.py"]
+
---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask('hello-world')
+@app.route('/')
+def hello():
+ return "Github Action CI/CD to Deploy Sample Python App on Google Cloud (Google Kubernetes Engine- GKE)\n"
+if __name__ == '__main__':
+ app.run(host = '0.0.0.0', port = 8080)
