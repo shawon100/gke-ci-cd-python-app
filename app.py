da0a159063af159bae92903899508e0f82482fae
Ashadullah Shawon <ashadullah.shawon@bjitgroup.com>
2022-06-23 10:53:47 +0600

    Updated content

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "Github Action CI/CD to Deploy Sample Python App on Google Cloud (Google Kubernetes Engine-GKE)\n"
+ return "CloudageSkill Microservices\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
